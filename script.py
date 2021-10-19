8c4b263470095a7c5a9f190cffa6d7cfbe40350d
Sahil Singh <sahilsingh08062001@gmail.com>
2021-10-19 11:47:33 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import re
 
+
 def removeSpaces(str):
     return str.strip()
 
@@ -76,10 +77,24 @@ def StringManipulate(str,master,check):
                 numStr.strip()
                 numStr = re.sub(' +', ' ', numStr)
                 num=numStr.split(" ")
-                c6=(num[0])
-                c7=(num[1])
-                c8=(num[2])
-                c9=(num[3])
+                # print(num)
+                try:
+                    c6=num[0]
+                except IndexError:
+                    c6="N/A"
+                try:
+                    c7 = num[0]
+                except IndexError:
+                    c7 = "N/A"
+                try:
+                    c8 = num[0]
+                except IndexError:
+                    c8 = "N/A"
+                try:
+                    c9 = num[0]
+                except IndexError:
+                    c9 = "N/A"
+                
 
                 textStr=str[0:i]
                 break
@@ -98,12 +113,6 @@ def StringManipulate(str,master,check):
     c9=removeSpaces(c9)
     textStr=removeSpaces(textStr)
     
-    print(textStr)
-
-   
-
-
-    
 
     outputData=[c1,c2,c3,c4,c5,c6,c7,c8,c9,textStr]
     return outputData
@@ -120,10 +129,12 @@ def StringTrim(data_list):
     output=[[]]
     master=""
     for str in data_list:
+        removeSpaces(str)
         if(str[0].isdigit()):
             master=str[:6]
         else:
             str = re.sub(r"^\s+", "", str)
+            # print(str)
             if(str[0].isdigit()):
                 outputData=StringManipulate(str,master,0)
                 output.append(outputData)
@@ -137,26 +148,30 @@ def StringTrim(data_list):
 
 def main():
 
-    # path=input("Enter path of file to be executed:")
-    # fileName=""
-    # for i in reversed(range(len(path))):
-    #     if(path[i]=="/"):
-    #         break
-    #     else:
-    #         fileName=path[i]+fileName
-    # fileName.replace("txt","xlsx")
 
-    df = pd.read_csv(
-        r'C:\Users\sahil\OneDrive\Desktop\Oliver\1990to1991CountyMigration\1990to1991CountyMigrationInflow\C9091aki.txt', sep='\t', names=["Data"])
-    df.to_excel(r'output.xlsx',index=None)
-    data_list=df["Data"].tolist()
-    output=StringTrim(data_list)
-    # print(output)
-    # print(data_list)
-    # print(StringTrim(data_list[2]))
+    #this list contains the keywords for the inflow txt files
+    f=['aki','ali','ari','azi','cai','coi','cti','dci','dei','fli','fri','gai','hii','iai','idi','ili','ini','ksi','kyi','lai','mai','mdi','mei','mii','mni',
+    'moi','msi','mti','nci','ndi','nei','nhi','nji','nmi','nvi','nyi','ohi','oki','ori','pai','rii','sci','sdi','tni','txi','uti','vai','vti','wai','wii','wvi','wyi']
 
-    df = pd.DataFrame.from_records(output,columns=["C1","C2","C3","C4","C5","C6","C7","C8","C9","Text"])
-    df.to_excel(r'C:\Users\sahil\OneDrive\Desktop\Oliver\Output\1990to1991\I\C9091aki.xlsx', index=None)
+    #this list contains the keywords for the inflow txt files
+    g = ['ako', 'alo', 'aro', 'azo', 'cao', 'coo', 'cto', 'dco', 'deo', 'flo', 'fro', 'gao', 'hio', 'iao', 'ido', 'ilo', 'ino', 'kso', 'kyo', 'lao', 'mao', 'mdo', 'meo', 'mio', 'mno',
+         'moo', 'mso', 'mto', 'nco', 'ndo', 'neo', 'nho', 'njo', 'nmo', 'nvo', 'nyo', 'oho', 'oko', 'oro', 'pao', 'rio', 'sco', 'sdo', 'tno', 'txo', 'uto', 'vao', 'vto', 'wao', 'wio', 'wvo', 'wyo']
+
+    #this for loop is used to run all the files at once
+    for i in range(52):
+
+        #change the input and output path of the txt and xlsx files respectively according to your need
+        inPath = r"C:\Users\sahil\OneDrive\Desktop\Oliver\1991to1992CountyMigration\1991to1992CountyMigrationOutflow\C9192"+g[i]+".txt"
+        outPath = r"C:\Users\sahil\OneDrive\Desktop\Oliver\Output\1991to1992\O\C9192"+g[i]+".xlsx"
+
+        df = pd.read_csv(inPath, sep='\t', names=["Data"])
+        df.to_excel(r'output.xlsx',index=None)
+        data_list=df["Data"].tolist()
+        output=StringTrim(data_list)
+
+
+        df = pd.DataFrame.from_records(output,columns=["C1","C2","C3","C4","C5","C6","C7","C8","C9","Text"])
+        df.to_excel(outPath, index=None)
 
 
 
